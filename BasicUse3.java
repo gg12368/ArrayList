8760f6c569a4c4a6dcafd75f4a57343394dfdc82
gg12368 <56186550+gg12368@users.noreply.github.com>
2020-02-05 18:38:31 +0800

    Create BasicUse3.java

---
+++ BasicUse3.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class ArrayListMethod {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        System.out.println(list);
+    }
+}
